33016bc7002f8443012589dcc2f25d169746a62c
projas <projas@kometsales.com>
2020-05-12 00:51:42 -0500

    Metodo Get , Consulta a base de datos

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+class UsersController extends Controller
+{
+    function index(){
+        $users = User::all();
+        return response()->json([$users],200);
+    }
+}
